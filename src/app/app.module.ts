feb45ab6cbc7abe7c68bc627da03db51e1b9a996
DXF0530 <dmitry.fleish@sbdinc.com>
2020-05-16 20:54:00 +0300

    commit 0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
     declarations: [
@@ -12,7 +17,16 @@ import { CoreModule } from './core/core.module';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        CoreModule
+        CoreModule,
+        StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        StoreRouterConnectingModule.forRoot()
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/reducers/index.ts
@@ -0,0 +1,36 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import * as fromRouter from '@ngrx/router-store';
+
+export interface State {
+  router: fromRouter.RouterReducerState<any>
+}
+
+export const reducers: ActionReducerMap<State> = {
+  router: fromRouter.routerReducer 
+};
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+
+export const selectRouter = createFeatureSelector<
+  State,
+  fromRouter.RouterReducerState<any>
+>('router');
+ 
+export const {
+  selectCurrentRoute,   // select the current route
+  selectQueryParams,    // select the current route query params
+  selectQueryParam,     // factory function to select a query param
+  selectRouteParams,    // select the current route params
+  selectRouteParam,     // factory function to select a route param
+  selectRouteData,      // select the current route data
+  selectUrl,            // select the current url
+} = fromRouter.getSelectors(selectRouter);
+
+export const getPlantType = createSelector(selectRouteParams('plantType'), (plantType) => plantType)
